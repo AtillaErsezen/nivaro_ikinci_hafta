69ebc447dc579ee8aa49bb37b1ec63fbf2063610
Atilla Ersezen <83984290+AtillaErsezen@users.noreply.github.com>
2023-09-15 20:51:18 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http")
+const server = http.createServer((req, res) =>{
+    res.writeHead(200, {"Content-Type" : "text/plain"});
+    res.end("node js is for backend");
+})
+const port = 3000;
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`)
+})
